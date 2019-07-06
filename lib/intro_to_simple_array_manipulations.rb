06902e34670b90bf7cf76bd1f177d61daf42db5c
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-06 09:58:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,16 @@
 def using_push(array, element)
   array.push(element)
 end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  using_pop(array)
+  array.pop
+end
