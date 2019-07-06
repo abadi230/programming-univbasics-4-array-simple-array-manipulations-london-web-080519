a4e466bdf4dc2b75249592d3cc2410f1d398b0fa
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-06 11:02:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,7 +42,6 @@ def using_delete(array, element)
   array.delete(element)
 end
 
-def using_delete_at(array, element)
-  index = array.index(element)
+def using_delete_at(array, index)
   array.delete_at(index)
 end
